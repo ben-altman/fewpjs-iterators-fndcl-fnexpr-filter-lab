c35770095336434861923e873d139ceaa95e6737
ben-altman <60247694+ben-altman@users.noreply.github.com>
2021-10-14 22:52:46 -0400

    commit tree 199987cbb68fb91fb5b70e8fd664844786a70063

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(arr, str) {
+    return arr.filter(d => d.toLowerCase() === str.toLowerCase());
+};
+
+function fuzzyMatch(arr, str) {
+    return arr.filter(d => d.substring(0, 2).toLowerCase() === str.toLowerCase() )
+};
+
+function matchName(arr, str) {
+    return arr.filter(d => d.name === str );
+};
